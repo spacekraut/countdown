73100de4996a6842095cf8489df30de8c807a73c
lukasilverio94 <lukasilverio94@gmail.com>
2023-11-12 01:07:18 +0100

    fixed animation/refactored functions

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-let alarmTime= 25
+//Initial variables
+let alarmTime = 25;
 let timer;
-let isRunning= false
-// Sound variable
+let isRunning = false;
 
+// Render Default Time at Screen
 $("#display-time").text(`${alarmTime}:00`);
 
 //Set Session
-const setSession = (alarmTime) => {
+const setSession = () => {
   $("#display-time").text(`
    ${alarmTime}:00`);
 };
-// Add and decrease Time
+// Update Time to Display
+const updateTime = () => {
+  $("#display-time").text(
+    `${remainedMinutes < 10 ? "0" + remainedMinutes : remainedMinutes} : ${
+      remainedSeconds < 10 ? "0" + remainedSeconds : remainedSeconds
+    }`
+  );
+};
+
+//Handle Click Increase Time Btn
 $("#add-time").click(() => {
   if (!isRunning) {
-   
     alarmTime++;
     setSession(alarmTime);
   }
 });
-
+//Handle Click Decrease Time Btn
 $("#decrease-time").click(() => {
   if (!isRunning && alarmTime > 1) {
     alarmTime--;
@@ -26,12 +35,12 @@ $("#decrease-time").click(() => {
   }
 });
 
-//Format Time
-const manageTime = (alarmTime) => {
-  seconds = alarmTime * 60 - 1;
-  remainedSeconds = seconds % 60;
-  remainedMinutes = Math.floor(seconds / 60);
-  return remainedMinutes, remainedSeconds;
+//Manage Time
+const manageTime = () => {
+  remainingSeconds = alarmTime * 60 - 1;
+  remainingMinutes = Math.floor(remainingSeconds / 60);
+  remainingSeconds %= 60;
+  return remainingMinutes, remainingSeconds;
 };
 
 // Toggle Buttons Hide/Show Reset/Start
@@ -43,11 +52,12 @@ const toggleButtons = (startBtn, resetBtn, isRunning) => {
 $("#start-btn").click(() => {
   isRunning = true;
   toggleButtons($("#start-btn"), $("#reset-btn"), isRunning);
+  setAnimation();
   startTimer(alarmTime);
 });
 
 $("#reset-btn").click(() => {
-  $(".container").toggleClass("changeColor");
+  $(".container").removeClass("changeColor");
   toggleButtons($("#start-btn"), $("#reset-btn"), false);
   isRunning = false;
   clearInterval(timer);
@@ -55,28 +65,24 @@ $("#reset-btn").click(() => {
   $("audio#audio")[0].pause();
 });
 
+// Handle Animation for container (CSS)
+const setAnimation = () => {
+  if (isRunning) {
+    $(".container").css("animation-duration", `${alarmTime * 60}s`);
+    $(".container").addClass("changeColor"); // Add the class to start the animation
+  }
+};
 
-
-
-const startTimer = (alarmTime) =>{
-
+// Start Timer
+const startTimer = (alarmTime) => {
   manageTime(alarmTime);
   let timeLeft = alarmTime * 60;
-  
-   //Animation
-  $(".container").css("animation-duration", `${alarmTime * 60}s`);
-  $(".container").toggleClass("changeColor");
-  
   timer = setInterval(() => {
     timeLeft--;
     if (timeLeft >= 0) {
-      second = timeLeft % 60;
-      minute = Math.floor(timeLeft / 60);
-      $("#display-time").text(
-        `${minute < 10 ? "0" + minute : minute} : ${
-          second < 10 ? "0" + second : second
-        }`
-      );
+      remainedSeconds = timeLeft % 60;
+      remainedMinutes = Math.floor(timeLeft / 60);
+      updateTime();
     } else {
       clearInterval(timer);
       $("audio#audio")[0].play();
